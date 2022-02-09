52cf9de332798675eb9253888fba9664d3c619f6
NatashaYumi <yumi-nakayama@hotmail.com>
2022-02-09 11:34:26 -0300

    Questão 1.4 Lista 1

---
+++ lista1_questao1.4.rb
@@ -0,0 +1,5 @@
+# Crie um script em Ruby que leia um número inteiro e mostre o dobro desse número.
+puts "Informe um número e pressione ENTER"
+numero = gets.chomp.to_i
+
+puts "O dobro de #{numero} é #{numero*2}"
