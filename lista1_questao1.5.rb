ea6e02e1af8f6d8c48788ad2fa6917e731800dd3
NatashaYumi <yumi-nakayama@hotmail.com>
2022-02-09 11:34:48 -0300

    Questão 1.5 Lista 1

---
+++ lista1_questao1.5.rb
@@ -0,0 +1,5 @@
+# Crie um script em Ruby que leia um número inteiro e mostre a metade desse número.
+puts "Informe um número e pressione ENTER"
+numero = gets.chomp.to_i
+
+puts "A metade de #{numero} é #{numero/2}"
