11ad2d90dce5c65c924177068c3a9405a624010a
NatashaYumi <yumi-nakayama@hotmail.com>
2022-02-11 14:14:20 -0300

    Questao 1.12 Lista 1

---
+++ lista1_questao1.12.rb
@@ -0,0 +1,7 @@
+# Crie um script em Ruby que leia dois números, X e Y e mostre o resto da divisão entre eles.
+print "Informe o primeiro número: "
+x = gets.chomp.to_i
+print "Informe o segundo número: "
+y = gets.chomp.to_i
+
+puts "Resto da divisão de #{x} e #{y} é #{x%y}"
